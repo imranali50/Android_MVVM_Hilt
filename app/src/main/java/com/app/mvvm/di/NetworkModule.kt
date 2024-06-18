5e37243a36198d5f5cf9dbfd59e6513bad0e4b3c
Hiren Ambaliya <hna@taglineinfotech.com>
2024-06-18 17:39:13 +0530

    api log and Authorization token added

--- app/src/main/java/com/app/mvvm/di/NetworkModule.kt
+++ app/src/main/java/com/app/mvvm/di/NetworkModule.kt
@@ -1,5 +1,6 @@
 package com.app.mvvm.di
 
+import android.util.Log
 import com.app.mvvm.remote.ApiService
 import com.app.mvvm.repository.Repository
 import com.app.mvvm.util.Constant
@@ -7,7 +8,10 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.Response
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
@@ -16,6 +20,7 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
+
     @Singleton
     @Provides
     fun provideHttpClient(): OkHttpClient {
@@ -23,7 +28,17 @@ object NetworkModule {
             .Builder()
             .readTimeout(15, TimeUnit.SECONDS)
             .connectTimeout(15, TimeUnit.SECONDS)
+            .addInterceptor(provideTokenInterceptor())
+            .addInterceptor(provideHttpLoggingInterceptor())
             .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+        return interceptor
     }
     @Singleton
     @Provides
@@ -45,5 +60,32 @@ object NetworkModule {
     @Provides
     fun provideCurrencyService(retrofit: Retrofit): ApiService =
         retrofit.create(ApiService::class.java)
+
+
+    class ServiceInterceptor : Interceptor {
+//        private var token : String = SplashActivity.jwtToken
+
+        override fun intercept(chain: Interceptor.Chain): Response {
+            var request = chain.request()
+            if(request.header("No-Authentication")==null){
+//                if(!token.isNullOrEmpty())
+//                {
+//                    val finalToken = token
+//                    Log.e("TAG", "intercept: >>>>>>>>>>>>>>>>>> $token")
+
+                    request = request.newBuilder()
+                        .addHeader("Authorization","finalToken")
+                        .build()
+//                }
+            }
+            return chain.proceed(request)
+        }
+    }
+    @Singleton
+    @Provides
+    fun provideTokenInterceptor(): ServiceInterceptor {
+        return ServiceInterceptor()
+    }
+
 
 }
